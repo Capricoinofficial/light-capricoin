814acd449a39ac4c64d74a57ff85195385622b47
Capricoin Developer <domains@inetsite.com>
2017-02-01 21:45:47 +0100

    remove plugins menu
    Nothing useful in the plugins menu

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -386,7 +386,7 @@ class LightWindow(QMainWindow):
         # Settings / Preferences are all reserved keywords in OSX using this as work around
         tools_menu.addAction(_("Light preferences") if sys.platform == 'darwin' else _("Preferences"), self.settings_dialog)
         tools_menu.addAction(_("&Network"), self.run_network_dialog)
-        tools_menu.addAction(_("&Plugins"), self.plugins_dialog)
+        #tools_menu.addAction(_("&Plugins"), self.plugins_dialog)
         tools_menu.addSeparator()
         tools_menu.addAction(_("&Sign/verify message"), self.sign_verify_message)
         tools_menu.addAction(_("&Encrypt/decrypt message"), self.encrypt_message)
